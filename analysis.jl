d7a587c3a93de88355aa61b9e7d4639d7cb2c78e
kevintram <thekevintram@gmail.com>
2021-02-27 09:32:35 -0500

    Builds Name Index BiMap

--- analysis.jl
+++ analysis.jl
@@ -4,25 +4,43 @@ using Pkg
 # Pkg.add("DBInterface")
 # Pkg.add("DataFrames")
 # Pkg.add("Query")
+# Pkg.add("Bijections")
 
 using SQLite
 using DBInterface
 using DataFrames
 using Query
 using DataFramesMeta
+using Bijections
 
 namesDB = SQLite.DB("names.db")
 
 namesDF = DBInterface.execute(namesDB, "SELECT * FROM names") |> DataFrame
 
-girlsDF = @linq namesDF |>
+# get all unique girl names
+girlsDF = unique(@linq namesDF |>
         where(:sex .== "F") |>
-        select(:name, :year)
+        select(:name))
 
-boysDF = @linq namesDF |>
+# get all unique boys names
+boysDF = unique(@linq namesDF |>
         where(:sex .== "M") |>
-        select(:name, :year)
+        select(:name))
 
-nG = nrow(unique(girlsDF,[:name]))
-nB = nrow(unique(boysDF,[:name]))
+nG = nrow(girlsDF)
+nB = nrow(boysDF)
 nY = nrow(unique(namesDF, [:year]))
+
+function createNameIndexBiMap(df)::Bijection{String, Int32}
+        bimap = Bijection{String,Int32}()
+        i = 1
+        for row in Tables.rows(df) 
+                bimap[row.name] = i
+                i += 1 # why does ++ not exist
+        end
+        bimap
+end;
+
+boyBiMap = createNameIndexBiMap(boysDF)
+girlBiMap = createNameIndexBiMap(girlsDF)
+
